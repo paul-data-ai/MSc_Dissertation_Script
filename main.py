77fa54d752bdd10c44cf70dca673068c5bd26927
paul <paullkingss@gmail.com>
2023-09-12 08:04:17 +0100

    API that returns route data with fares updated

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import json
 
 
 @app.route('/get_directions/', methods=['GET'])
-def ask_and_get():
+def get_directions():
     origin = request.args.get('origin')
     destination = request.args.get('destination')
     mode = request.args.get('mode', default=None)
@@ -48,31 +48,6 @@ def ask_and_get():
         api_response2 = get_api_response(api_url)
         route_options = api_response2['journeys']
         
-        # route_info = {}
-        # for index, route in enumerate(route_options):
-        #     key = f"route{index + 1}" 
-        #     route_info[key] = {
-        #         'startDateTime': route['startDateTime'],
-        #         'arrivalDateTime': route['arrivalDateTime'],
-        #         'est. duration': route['duration'],
-        #         'alternativeRoute': route['alternativeRoute'],
-        #         'legs': []
-        #     }
-            
-        #     for leg in route['legs']:
-        #         leg_info = {
-        #             'Origin': leg['departurePoint']['commonName'],
-        #             'Destination': leg['arrivalPoint']['commonName'],
-        #             'Summary': leg['instruction']['summary'],
-        #             'mode': leg['mode']['name'],
-        #             'google_maps_link': f"https://www.google.com/maps/dir/?api=1&origin=?{quote(leg['departurePoint']['commonName'])}&destination={quote(leg['arrivalPoint']['commonName'])}&travelmode={leg['mode']['name']}"
-        #         }
-                
-        #         route_info[key]['legs'].append(leg_info)
-
-        # sorted_routes = sorted(route_info.values(), key=lambda x: x['est. duration'])
-
-        # sorted_route_info = {f"route{index + 1}": route for index, route in enumerate(sorted_routes)}
         route_info = {}
         for index, route in enumerate(route_options):
             route_info[index] = {
@@ -84,7 +59,8 @@ def ask_and_get():
             }
             
             for leg in route['legs']:
-                leg_info = OrderedDict()
+                # leg_info = OrderedDict()
+                leg_info = {}
                 leg_info['destination'] = leg['arrivalPoint']['commonName']
                 leg_info['_origin'] = leg['departurePoint']['commonName']
                 leg_info['__Summary'] = leg['instruction']['summary']
@@ -94,14 +70,66 @@ def ask_and_get():
                 route_info[index]['legs'].append(leg_info)
 
         sorted_routes = sorted(route_info.values(), key=lambda x: x['duration'])
+
         sorted_route_info = OrderedDict((f"route{index + 1}", route) for index, route in enumerate(sorted_routes))
 
-        # json_response = json.dumps(sorted_route_info, indent=4, sort_keys=True)
+        route_data_with_fares = estimate_fares(sorted_route_info, mode_cost_per_minute)
 
-        # print (route_info.keys())
-        return jsonify(sorted_route_info)
+        lowest_fare_route = get_lowest_fare_route(route_data_with_fares)
+
+        return jsonify(lowest_fare_route)
     else:
         return jsonify({'error': 'Try modifying the origin or destination'})
+
+
+
+from datetime import datetime
+def format_timestamp(date_string):
+
+    # Parse the string to a datetime object
+    date_object = datetime.fromisoformat(date_string)
+
+    # Format the datetime object to a more user-friendly string
+    info = date_object.strftime("%A, %B %d, %Y at %I:%M %p")    
+    return info
+
+
+def get_lowest_fare_route(data):
+    lowest_fare_route = min(data.values(), key=lambda x: x['estimated_fare'])
+    _status = (f"The route with the lowest fare is route with start time {format_timestamp(lowest_fare_route['startBy'])} and fare £{lowest_fare_route['estimated_fare']}")
+    return {'_status': _status, 'lowest_fare_route' : lowest_fare_route}
+
+def estimate_fares(route_data, mode_cost_per_minute):
+    for route_name, route_details in route_data.items():
+        total_fare = 0
+        for leg in route_details["legs"]:
+            mode = leg["mode"]
+            # Assuming each leg's duration can be calculated based on start and end times of the route
+            leg_duration = route_details["duration"] / len(route_details["legs"])
+            
+            # Calculate fare for this leg based on the mode
+            # If the mode is walking, the cost is zero
+            total_fare += leg_duration * mode_cost_per_minute.get(mode, 0)
+        
+        # Add total fare to route details
+        route_details["estimated_fare"] = round(total_fare, 2)
+    
+    return route_data
+
+
+# Defining an hypothetical average cost per minute for each mode
+mode_cost_per_minute = {
+    "bus": 0.15,  
+    "overground": 0.20,  
+    "tube": 0.25,  
+    "dlr": 0.20, 
+    "tram": 0.15,  
+    "tflrail": 0.20, 
+    "river": 0.50, 
+    "walking": 0.00,  
+    "cycling": 0.05, 
+    "taxi": 0.60
+}
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
